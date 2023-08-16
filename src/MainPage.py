401a6495a9b6b70eb521df5029ad156c4d565e67
C-rrente <logan6656@gmail.com>
2023-08-16 12:24:27 -0700

    Cleanup

--- src/MainPage.py
+++ src/MainPage.py
@@ -70,7 +70,7 @@ class MainPage(Frame):
             anchor="nw",
             text="UCSD Makerspace",
             fill="#F5F0E6",
-            font=("Montserrat bold", 113 * -1)
+            font=("Montserrat", 113 * -1, 'bold')
         )
 
         canvas.create_text(
