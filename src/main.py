f3d5dfc7ec749cb47fbc72598f6b8ddbc144f8c2
Kyle Huang <kylehuang06@gmail.com>
2025-02-26 17:34:21 -0800

    Fix syntax error

--- src/main.py
+++ src/main.py
@@ -158,7 +158,7 @@ if __name__ == "__main__":
     global_.traffic_light.set_off()
     sw = swipe()
     util = utils()
-    thread = Thread(target=myLoop, args=(app))
+    thread = Thread(target=myLoop, args=(app, ))
     logging.info("Starting thread")
     thread.start()
     app.bind("<Key>", lambda i: sw.keyboardPress(i))
