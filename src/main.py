de9443575a5a17d0824370329087c43d1522cec0
hermano727 <hh727w@gmail.com>
2025-06-18 15:40:31 -0700

    convert user lookup to  old logic for now

--- src/main.py
+++ src/main.py
@@ -92,17 +92,17 @@ def myLoop(app, reader):
             ##############################
             # Use local DB for user data #
             ##############################
-            try:
-                with open("assets/local_user_db.json", "r", encoding="utf-8") as f:
-                    user_data = json.load(f)
-            except FileNotFoundError:
-                logging.error("Local user database not found. Please run export_user_db.py to create it.")
+            # try:
+            #     with open("assets/local_user_db.json", "r", encoding="utf-8") as f:
+            #         user_data = json.load(f)
+            # except FileNotFoundError:
+            #     logging.error("Local user database not found. Please run export_user_db.py to create it.")
 
             # # Get a list of all users
-            # user_data = global_.sheets.get_user_db_data()
+            user_data = global_.sheets.get_user_db_data()
 
             # # Get a list of all waiver signatures
-            # waiver_data = global_.sheets.get_waiver_db_data()
+            waiver_data = global_.sheets.get_waiver_db_data()
 
             curr_user = "None"
             curr_user_w = "None"
