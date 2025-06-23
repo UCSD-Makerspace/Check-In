3e642656aaf095948a5af0a6d5ef44a28dc5c994
hermano727 <hh727w@gmail.com>
2025-06-23 14:16:13 -0700

    fix reference before assignment for 'waiver_updated'

--- src/main.py
+++ src/main.py
@@ -126,6 +126,7 @@ def myLoop(app, reader):
                     # User has not checked in since local DB was created
                     needs_refresh = True
 
+                waiver_updated = False
                 if waiver_signed != "true":
                     logging.info("Waiver not found locally for " + curr_user["Name"]
                                 + " with PID " + curr_user["Student ID"] + " at " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
