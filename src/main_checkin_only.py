e2ac1c47289e6bbfea98d2089f824dd8ba0c0f57
hermano727 <hh727w@gmail.com>
2025-06-24 15:16:45 -0700

    convert checkin_only to new system

--- src/main_checkin_only.py
+++ src/main_checkin_only.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from checkin_queue import CheckInLogger
 from gui import *
 from reader import *
 from fabman import *
@@ -63,7 +64,9 @@ def myLoop(app, reader):
                     no_wifi.after(4000, lambda: destroyNoWifiError(no_wifi))
                 continue
 
+            tag_read_start = perf_counter()
             tag = reader.grabRFID()
+            tag_read_end = perf_counter()
 
             if " " in tag:
                 continue
@@ -85,91 +88,155 @@ def myLoop(app, reader):
             ##############################
             # Use local DB for user data #
             ##############################
+            t0 = perf_counter()
             try:
                 with open("assets/local_user_db.json", "r", encoding="utf-8") as f:
                     user_data = json.load(f)
             except FileNotFoundError:
                 logging.error("Local user database not found. Please run export_user_db.py to create it.")
                 continue 
-
+            t1 = perf_counter()
             curr_user = user_data.get(tag, None)
             curr_user_w = "None"
+            t2 = perf_counter()
+            t3 = t4 = t5 = t6 = t7 = None
 
-            user_id = ""
-            if curr_user != "None":
-                waiver_data = global_.sheets.get_waiver_db_data()
-                for i in waiver_data:
-                    if not isinstance(i, dict) or "A_Number" not in i or "Email" not in i:
-                        logging.warning("Invalid waiver data format")
-                        util.showTempError(frame = global_.app.get_frame(MainPage), message="ERROR. Please tap again")
-                        continue
+            ###################################
+            # Case handling for user check-in #      
+            ################################### 
+            if curr_user:
+                user_id = curr_user["Student ID"].strip().lower()
+                waiver_signed = curr_user.get("Waiver Signed", "").strip().lower()
+                firstEnrTrm = curr_user["firstEnrTrm"]
+                lastEnrTrm = curr_user["lastEnrTrm"] 
 
-                    waiver_id = i["A_Number"].lower().replace("+e?", "")[:9]
-                    waiver_email = i["Email"].lower()
+                # Used to check if firstEnrTrm and lastEnrTrm are stale and need to be updated
+                last_checked_in_str = curr_user.get("lastCheckIn")
+                needs_refresh = False
 
-                    user_id = curr_user["Student ID"].lower().replace("+e?", "")[:9]
-                    user_email = curr_user["Email Address"].lower()        
+                if last_checked_in_str:
+                    last_checked_in_date = dt.strptime(last_checked_in_str, "%Y-%m-%d").date()
+                    today = dt.today().date()
+                    diff = (today - last_checked_in_date).days
+                    if diff >= 21:
+                        needs_refresh = True
+                else:
+                    # User has not checked in since local DB was created
+                    needs_refresh = True
 
-                    if user_id[0] == "a":
-                        user_id = user_id[1:]
-                    if waiver_id[0] == "a":
-                        waiver_id = waiver_id[1:]
+                if not curr_user.get("firstEnrTrm") or not curr_user.get("lastEnrTrm"):
+                    needs_refresh = True
 
-                    if user_id == waiver_id or user_email == waiver_email:
-                        curr_user_w = i
-                        break
+                waiver_updated = False
 
-            # Used to grab firstEnrTrm and lastEnrTrm
-            student_info = contact.get_student_info_pid("A" + user_id)
-            if student_info:
-                firstEnrTrm = student_info[4]
-                lastEnrTrm = student_info[5]
+                if waiver_signed != "true":
+                    logging.info("Waiver not found locally for " + curr_user["Name"]
+                                + " with PID " + curr_user["Student ID"] + " at " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                    waiver_data = global_.sheets.get_waiver_db_data()
+                    if utils.check_waiver_match(curr_user, waiver_data):
+                        t4 = perf_counter()
+                        logging.info("Waiver found online for " + curr_user["Name"])
+                        curr_user["Waiver Signed"] = "true"
+                        curr_user["Student ID"] = "A" + user_id.lstrip("a")
+                        curr_user_w = "waiver_confirmed"
+                        waiver_updated = True
+                else:
+                    logging.info("Account & waiver found locally for " + curr_user["Name"] + " at " + time.strftime("%H:%M:%S", time.localtime()))
+                    curr_user_w = "waiver_confirmed"
+
+                if needs_refresh:
+                    logging.info("Updating firstEnrTrm and lastEnrTrm for " + curr_user["Name"])
+                    t5 = perf_counter()
+                    student_info = contact.get_student_info_pid("A" + user_id.lstrip("a"))
+                    t6 = perf_counter()
+                    if student_info:
+                        curr_user["firstEnrTrm"] = student_info[4]
+                        curr_user["lastEnrTrm"] = student_info[5]
+                    curr_user["lastCheckIn"] = dt.today().strftime("%Y-%m-%d")
+
+                # Writing conditions: Either we update enrolled terms or local waiver status
+                if waiver_updated or needs_refresh:
+                    user_data[tag] = curr_user
+                    with open("assets/local_user_db.json", "w", encoding="utf-8") as f:
+                        json.dump(user_data, f, indent=2)
+
+            # If user is not found locally, check the online user DB.
+            # If found online, check waiver status and append both to local DB.
+            # Else, redirect user to usual account creation page
             else:
-                logging.warning(f"API timeout for user_id: {user_id}")
-                util.showTempError(frame = global_.app.get_frame(MainPage), message="ERROR. Please tap again in 3 seconds")
-                continue
+                logging.info("User not found in local DB, checking with online database")
+                user_data_online = global_.sheets.get_user_db_data()
+                t3 = perf_counter()
+                waiver_data = global_.sheets.get_waiver_db_data()
+                for i in user_data_online:
+                    if i["Card UUID"] == tag:
+                        curr_user = i
+                        user_id = curr_user["Student ID"].lower()
+                        break
+                if curr_user and utils.check_waiver_match(curr_user, waiver_data):
+                    logging.info(f"User found online: {curr_user['Name']} but not locally at " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                    
+                    # Write user to local database if we find the user online but not locally
+                    user_data[tag] = {
+                        "Name": curr_user["Name"],
+                        "Student ID": "A" + user_id.lstrip("a"),
+                        "Email Address": curr_user["Email Address"],
+                        "Waiver Signed": "true",
+                    }
 
-            ############################
-            # All scenarios for ID tap #
-            ############################
+                    with open("assets/local_user_db.json", "w", encoding="utf-8") as f:
+                        json.dump(user_data, f, indent=2)
+                    curr_user_w = "waiver_confirmed"
+                    logging.info(f"Updated local DB with user: {curr_user['Name']}")  
 
-            if curr_user == "None" and curr_user_w == "None":
-                # for check-in only they cannot make an account
-                logging.info("User was not found in the database")
-                app.show_frame(NoAccCheckInOnly)
-                app.after(5000, lambda: app.show_frame(MainPage))
+            if curr_user is None and curr_user_w == "None":
+                logging.info("User was not found in online database")
+                global_.traffic_light.set_red()
+                app.show_frame(NoAccNoWaiver)
+                app.after(3000, lambda: app.show_frame(NoAccNoWaiverSwipe))
             elif curr_user_w == "None":
                 logging.info("User does not have waiver")
+                global_.traffic_light.set_yellow()
                 app.show_frame(AccNoWaiver)
                 app.after(3000, lambda: app.show_frame(AccNoWaiverSwipe))
+            elif curr_user is None:
+                logging.info("User has a waiver but no account")
+                app.show_frame(WaiverNoAcc)
+                app.after(3000, lambda: app.show_frame(WaiverNoAccSwipe))
             else:
-                student_info = contact.get_student_info_pid("A" + user_id)
-                if student_info:
-                    firstEnrTrm = student_info[4]
-                    lastEnrTrm = student_info[5]
-                    new_row = [
+                new_row = [
                     util.getDatetime(),
                     int(time.time()),
                     curr_user["Name"],
                     str(tag),
                     "User Checkin",
-                    "Check-in Only",
+                    "Main Check-in",
                     firstEnrTrm,
                     lastEnrTrm, 
-                    ]
-                    activity_log = global_.sheets.get_activity_db()
-                    activity_log.append_row(new_row)
-                    global_.app.get_frame(UserWelcome).displayName(curr_user["Name"])
-                else:
-                    logging.warning(f"API timeout for user_id: {user_id}")
-                    util.showTempError(global_.app.get_frame(MainPage), message="ERROR. Please tap again in 3 seconds")
-                    continue
+                ]
+                # Add to check-in queue
+                t7 = perf_counter()
+                checkin_logger.enqueue_row(new_row, tag)
+                global_.traffic_light.set_green()
+                global_.app.get_frame(UserWelcome).displayName(curr_user["Name"])
+                logging.info("[TIMING BREAKDOWN]")
+                logging.info(f"RFID read time: {tag_read_end - tag_read_start:.4f}s")
+                logging.info(f"Load local_user_db.json: {t1 - t0:.4f}s")
+                logging.info(f"Local user lookup: {t2 - t1:.4f}s")
+                if t3 is not None and t4 is not None:
+                    logging.info(f"Online waiver check: {t4 - t3:.4f}s")
+                if t5 is not None and t6 is not None:
+                    logging.info(f"Student info API call: {t6 - t5:.4f}s")
+                if t6 is not None and t7 is not None:
+                    logging.info(f"Enqueue + display: {t7 - t6:.4f}s")
+                elif t2 is not None and t7 is not None:
+                    logging.info(f"Total check-in time (no API): {t7 - tag_read_start:.4f}s")
+                logging.info(f"Total time from RFID scan to display: {t7 - tag_read_start:.4f}s")
 
             last_time = time.time()
             last_tag = tag
 
             reader.readSerial()
-
 
 def destroyNoWifiError(no_wifi):
     global no_wifi_shown
@@ -207,6 +274,7 @@ if __name__ == "__main__":
     global_.init()
     app = gui()
     global_.setApp(app)
+    checkin_logger = CheckInLogger()
     reader = Reader()
     util = utils()
     thread = Thread(target=myLoop, args=(app, reader))
