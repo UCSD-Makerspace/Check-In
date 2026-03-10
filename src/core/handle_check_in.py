bc054801084bd0239650a3f012cf15f679fb82a6
Timothy <timothyrwashburn@gmail.com>
2026-03-09 21:28:57 -0700

    merge new_row_check_in.py into handle_check_in.py

--- src/core/handle_check_in.py
+++ src/core/handle_check_in.py
@@ -1,20 +1,40 @@
 import global_
 import logging
+import time
 from core.write_checkin import write_checkin
-from core.new_row_check_in import new_row_check_in
 from core.UserRecord import UserRecord
 from gui import *
-from reader import *
+
 
 def handle_check_in(tag, util):
     curr_user = UserRecord.load(tag, global_.sheets)
 
     if not curr_user:
         logging.info(f"User {tag} not found.")
-        new_row_check_in(None, "None", tag, util, None, None)
+        global_.traffic_light.set_red()
+        global_.app.show_frame(NoAccNoWaiver)
+        global_.app.after(3000, lambda: global_.app.show_frame(NoAccNoWaiverSwipe))
         return
 
-    waiver_status = "waiver_confirmed" if curr_user.has_waiver() else "None"
+    if not curr_user.has_waiver():
+        logging.info(f"User {curr_user.data['Name']} does not have waiver")
+        global_.traffic_light.set_yellow()
+        global_.app.show_frame(AccNoWaiver)
+        global_.app.after(3000, lambda: global_.app.show_frame(AccNoWaiverSwipe))
+        return
 
-    new_row_check_in(curr_user.data, waiver_status, tag, util, curr_user.data.get("firstEnrTrm"), curr_user.data.get("lastEnrTrm"))
+    new_row = [
+        util.getDatetime(),
+        int(time.time()),
+        curr_user.data["Name"],
+        str(tag),
+        "User Check-In",
+        "",
+        curr_user.data.get("firstEnrTrm", ""),
+        curr_user.data.get("lastEnrTrm", ""),
+    ]
+
+    global_.checkin_logger.enqueue_row(new_row, tag)
+    global_.traffic_light.set_green()
+    global_.app.get_frame(UserWelcome).displayName(curr_user.data["Name"])
     write_checkin(curr_user.data, tag)
