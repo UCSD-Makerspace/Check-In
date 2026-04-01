3a048c53524020e498415734018fbf5ef7536ba9
Timothy <timothyrwashburn@gmail.com>
2026-03-31 23:31:47 -0700

    move off main thread, improve logging

--- src/controllers/account_controller.py
+++ src/controllers/account_controller.py
@@ -1,7 +1,7 @@
 import logging
+from threading import Thread
 
 from PyQt6.QtCore import QTimer
-from PyQt6.QtWidgets import QApplication
 
 from controllers.api_controller import ApiController
 
@@ -12,16 +12,19 @@ class AccountController:
 
     def go_to_review_from_barcode(self, barcode):
         self.ctx.nav.show_status("Looking up student...")
-        QApplication.processEvents()
+        logging.info(f"Looking up student by barcode: {barcode}")
+        Thread(target=self._lookup_barcode_worker, args=(barcode,), daemon=True).start()
 
+    def _lookup_barcode_worker(self, barcode):
         student = ApiController.lookup_by_barcode(barcode)
-        self.ctx.nav.hide_status()
+        self.ctx.dispatcher.call.emit(lambda s=student: self._on_barcode_result(s))
 
+    def _on_barcode_result(self, student):
+        self.ctx.nav.hide_status()
         if student is None:
             self.ctx.nav.show_status("Student not found. Please enter your details manually.")
             QTimer.singleShot(3000, self.ctx.nav.hide_status)
             return
-
         self.ctx.nav.go_to_create_account_review(
             pid=student["pid"],
             first_name=student["first_name"],
@@ -31,16 +34,19 @@ class AccountController:
 
     def go_to_review_from_pid(self, pid):
         self.ctx.nav.show_status("Looking up student...")
-        QApplication.processEvents()
+        logging.info(f"Looking up student by PID: {pid}")
+        Thread(target=self._lookup_pid_worker, args=(pid,), daemon=True).start()
 
+    def _lookup_pid_worker(self, pid):
         student = ApiController.lookup_by_pid(pid)
-        self.ctx.nav.hide_status()
+        self.ctx.dispatcher.call.emit(lambda s=student: self._on_pid_result(s, pid))
 
+    def _on_pid_result(self, student, pid):
+        self.ctx.nav.hide_status()
         if student is None:
             self.ctx.nav.show_status("Student not found. Please check your PID.")
             QTimer.singleShot(3000, self.ctx.nav.hide_status)
             return
-
         self.ctx.nav.go_to_create_account_review(
             pid=pid,
             first_name=student["first_name"],
@@ -56,8 +62,14 @@ class AccountController:
 
     def _create(self, *, barcode=None, pid=None, first_name=None, last_name=None, email=None):
         self.ctx.nav.show_status("Account creation in progress!")
-        QApplication.processEvents()
+        logging.info(f"Creating account: pid={pid} barcode={barcode}")
+        Thread(
+            target=self._create_worker,
+            kwargs=dict(barcode=barcode, pid=pid, first_name=first_name, last_name=last_name, email=email),
+            daemon=True,
+        ).start()
 
+    def _create_worker(self, *, barcode, pid, first_name, last_name, email):
         result = ApiController.create_account(
             self.ctx.rfid,
             barcode=barcode,
@@ -66,12 +78,13 @@ class AccountController:
             last_name=last_name,
             email=email,
         )
-        self.ctx.nav.hide_status()
+        self.ctx.dispatcher.call.emit(lambda r=result: self._on_create_result(r))
 
+    def _on_create_result(self, result):
+        self.ctx.nav.hide_status()
         if result is None:
             self.ctx.nav.show_status("ERROR! Could not create account, please try manually.")
             QTimer.singleShot(3000, self.ctx.nav.hide_status)
             return
-
         logging.info("Account creation succeeded")
         self.ctx.nav.pop()
